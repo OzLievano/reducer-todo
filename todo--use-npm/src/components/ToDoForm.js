f737fec331b5182082de3f609815a265e84fa46c
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-11-11 22:25:57 -0500

    rendering state data to page

--- todo--use-npm/src/components/ToDoForm.js
+++ todo--use-npm/src/components/ToDoForm.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React,{useReducer} from 'react'
+import {formReducer,initialState} from '../reducers/formReducer'
 
 const ToDoForm = () => {
+
+    const [state,dispatch] = useReducer(formReducer,initialState)
+
+    console.log(state)
     return (
         <div>
            <form>
             <label htmlFor="taskName">
                 <input type="text" name="taskName" id="taskName"/>
             </label>
-           </form> 
+           </form>
+           <div>
+               {state.map((item)=>{
+                   return (
+                   <div>
+                    {item.item},{item.id},{item.complete}
+                   </div>
+                   )
+               })}
+           </div> 
         </div>
     )
 }
